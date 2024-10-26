ac48d7d51d1fe867c69cc48d216c15be19648b1c
Romain <98345704+Romlion@users.noreply.github.com>
2024-10-26 20:25:30 +0200

    all modification for the second commit

---
+++ src/components/MonComposant.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MonComposant extends React.Component {
+    render() {
+        return (
+            <div>
+                nouveau composant 
+            </div>
+        )
+    }
+}
---
+++ src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SignUp({ email }) {
+    return (
+        <div>
+            <h2>Inscription</h2>
+            <p>Email: {email}</p>
+        </div>
+    )
+}
+
+export default SignUp
